50271f614e13917741da54e97901bcf96f0fd41f
Tyler Barnes <tylerdbarnes@gmail.com>
2019-10-22 21:22:43 -0700

    make it wpgraphql!

--- src/components/GraphiQL/index.js
+++ src/components/GraphiQL/index.js
@@ -11,7 +11,6 @@ import ToggleButton from '../ToggleButton'
 import 'graphiql/graphiql.css'
 import './style.css'
 
-
 class GraphiQLComponent extends Component {
   state = {
     render: false,
@@ -22,22 +21,19 @@ class GraphiQLComponent extends Component {
   constructor() {
     super()
 
-    uniqueId.enableUniqueIds(this);
-
+    uniqueId.enableUniqueIds(this)
   }
 
   componentDidMount() {
-
     this.setState({
       render: true,
     })
   }
 
-  fetcher = ( graphQLParams ) => {
+  fetcher = graphQLParams => {
+    const { endpoint, showQueryLog, showTracing } = this.props
 
-    const { endpoint, showQueryLog, showTracing } = this.props;
-
-    return fetch( endpoint ? endpoint : 'https://www.wpgraphql.com/graphql', {
+    return fetch(endpoint ? endpoint : 'https://www.wpgraphql.com/graphql', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
@@ -47,97 +43,92 @@ class GraphiQLComponent extends Component {
     })
       .then(response => response.json())
       .then(res => {
-
-        if (! showTracing) {
+        if (!showTracing) {
           delete res.extensions.tracing
         }
 
-        if (! showQueryLog) {
+        if (!showQueryLog) {
           delete res.extensions.queryLog
         }
 
-        if (Object.keys( res.extensions ).length === 0) {
+        if (Object.keys(res.extensions).length === 0) {
           delete res.extensions
         }
 
         return res
-      });
+      })
   }
 
   showJoyRide = id => {
-
-    const { withDocs, showJoyride } = this.props;
+    const { withDocs, showJoyride } = this.props
 
     let steps = [
       {
         target: '.' + id + ' .graphiql-container',
         title: 'GraphiQL IDE',
-        content: 'This is the GraphiQL IDE. It allows you to interact with a live WPGraphQL API',
+        content:
+          'This is the GraphiQL IDE. It allows you to interact with a live WPGraphQL API',
         placement: 'top',
       },
       {
         target: '.' + id + ' .topBar',
         title: 'Toolbar',
-        content: 'This is the GraphiQL Toolbar. It contains buttons to interact with the IDE.',
+        content:
+          'This is the GraphiQL Toolbar. It contains buttons to interact with the IDE.',
         placement: 'top',
       },
       {
         target: '.' + id + ' .execute-button',
         title: 'Execute Button',
-        content: 'This is the Execute Button. It will execute the operation (query or mutation) in the query editor.',
+        content:
+          'This is the Execute Button. It will execute the operation (query or mutation) in the query editor.',
         placement: 'top',
       },
       {
         target: '.' + id + ' .query-editor',
-        content: 'This is the Query Editor. You can write your GraphQL Queries and Mutations here.',
+        content:
+          'This is the Query Editor. You can write your GraphQL Queries and Mutations here.',
         placement: 'left',
       },
       {
         target: '.' + id + ' .result-window',
-        content: 'This is the Result Window. When a query is executed, the results are displayed here.',
+        content:
+          'This is the Result Window. When a query is executed, the results are displayed here.',
         placement: 'right',
       },
-    ];
+    ]
 
-    if ( withDocs ) {
-
+    if (withDocs) {
       const docSteps = [
         {
           target: '.' + id + ' .doc-explorer',
           content: 'This is the documentation explorer.',
-          placement: 'top'
+          placement: 'top',
         },
         {
           target: '.' + id + ' .search-box',
-          content: 'Enter keywords to search the Schema.'
+          content: 'Enter keywords to search the Schema.',
         },
         {
           target: '.' + id + ' .doc-category',
-          content: 'Interact with the Schema by clicking on Types and Fields to discover what is available in the Schema.'
-        }
-      ];
+          content:
+            'Interact with the Schema by clicking on Types and Fields to discover what is available in the Schema.',
+        },
+      ]
 
-      if ( 'docsOnly' === showJoyride ) {
-        steps = docSteps;
+      if ('docsOnly' === showJoyride) {
+        steps = docSteps
       } else {
         steps = [...steps, ...docSteps]
       }
-
     }
 
-    return (
-      <Joyride
-        steps={ steps }
-        continuous={true}
-        showProgress={true}
-      />
-    )
-
+    return <Joyride steps={steps} continuous={true} showProgress={true} />
   }
 
   setAuthHeader = (auth, key) => {
-    const { endpoint } = this.props;
-    fetch( endpoint ? endpoint : 'https://www.wpgraphql.com/graphql', {
+    const { endpoint } = this.props
+    fetch(endpoint ? endpoint : 'https://www.wpgraphql.com/graphql', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json',
@@ -158,23 +149,23 @@ class GraphiQLComponent extends Component {
           clientId: v4(),
           user: auth[0],
           pass: auth[1],
-        }
+        },
       }),
     })
-    .then(response => response.json())
-    .then((res) => {
-      const token = get(res, 'data.login.authToken')
-      if (token) {
-        this.setState({
-          headers: { Authorization: `Bearer ${token}` },
-          selected: { [key]: 'green' },
-        })
-      }
-    })
-    .catch(err => {
-      this.setState({ selected: { [key]: 'red' } })
-      console.error(err)
-    })
+      .then(response => response.json())
+      .then(res => {
+        const token = get(res, 'data.login.authToken')
+        if (token) {
+          this.setState({
+            headers: { Authorization: `Bearer ${token}` },
+            selected: { [key]: 'green' },
+          })
+        }
+      })
+      .catch(err => {
+        this.setState({ selected: { [key]: 'red' } })
+        console.error(err)
+      })
 
     this.setState({ selected: { [key]: 'yellow' } })
   }
@@ -191,7 +182,7 @@ class GraphiQLComponent extends Component {
     } = this.props
     const formattedQuery = print(parse(query))
     const showDocs = true !== withDocs ? 'without-docs' : null
-    const uniqid = this.nextUniqueId();
+    const uniqid = this.nextUniqueId()
 
     return (
       <React.Fragment>
@@ -203,30 +194,30 @@ class GraphiQLComponent extends Component {
             label={`Login As ${key}`}
           />
         ))}
-        {
-          render
-          ? <div
-              className={classNames('graphiql-wrapper', showDocs, uniqid)}
-              style={{
-                height: '500px',
-                width: '100%',
-                margin: '15px 0',
-                padding: '0',
+        {render ? (
+          <div
+            className={classNames('graphiql-wrapper', showDocs, uniqid)}
+            style={{
+              height: '500px',
+              width: '100%',
+              margin: '15px 0',
+              padding: '0',
+            }}
+          >
+            {showJoyride ? this.showJoyRide(uniqid) : null}
+            <GraphiQL
+              fetcher={this.fetcher}
+              query={formattedQuery}
+              variables={variables ? JSON.stringify(variables, null, 2) : null}
+              extensions={{
+                queryLog: true,
+                tracing: false,
               }}
-            >
-              { showJoyride ? this.showJoyRide( uniqid ) : null }
-              <GraphiQL
-                fetcher={ this.fetcher }
-                query={formattedQuery}
-                variables={variables ? JSON.stringify(variables, null, 2) : null}
-                extensions={{
-                  queryLog: true,
-                  tracing: false
-                }}
-              />
-            </div>
-          : 'GraphiQL Loading...'
-        }
+            />
+          </div>
+        ) : (
+          'GraphiQL Loading...'
+        )}
       </React.Fragment>
     )
   }
